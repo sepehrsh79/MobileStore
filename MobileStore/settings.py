fbd2694a05a3623fe3f8d52392892eea74ac58c6
sepehrsh <sepehrshafiee1379@gmail.com>
2021-04-29 17:06:19 +0430

    third change

--- MobileStore/settings.py
+++ MobileStore/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'wu4+ta@tabol(md@rq=aftyofe(19i08&*)(!@-x8-m3c4lyxy'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['localhost', '127.0.0.1']
 
